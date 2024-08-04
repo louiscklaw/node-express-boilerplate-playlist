38f95cc906b65302a128d82058da098ff3357f89
Louis Law, louiscklaw <louiscklaw@users.noreply.github.com>
2024-08-04 13:13:28 +0800

    update,

--- restaurant_user-tryout/dc_up.sh
+++ restaurant_user-tryout/dc_up.sh
@@ -5,6 +5,6 @@ set -ex
 # yarn docker:dev
 
 docker compose -f docker-compose.yml -f docker-compose.dev.yml build --no-cache
-docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+docker compose -f docker-compose.yml -f docker-compose.dev.yml up
 
-docker compose -f docker-compose.yml -f docker-compose.dev.yml exec -it node-app bash
+# docker compose -f docker-compose.yml -f docker-compose.dev.yml exec -it node-app bash
---
+++ restaurant_user-tryout/file_modify.md
@@ -0,0 +1,23 @@
+start and prove the original work
+
+```bash
+$ ./dc_up.sh
+```
+
+highlight customer, Customer
+
+
+
+prompt
+```
+modify `customer` to `restaurant_user` accordingly, thanks
+```
+
+
+```
+./src/routes/v1/customer.route.js
+./src/validations/customer.validation.js
+./src/controllers/customer.controller.js
+./src/services/customer.service.js
+./src/models/customer.model.js
+```
